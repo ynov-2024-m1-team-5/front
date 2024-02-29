7d89c178044bd26240b8e983066faacd79cc7361
MalicknND <ndiayemalick062@gmail.com>
2024-02-29 14:42:41 +0100

    integration de la page produts et product

--- src/components/products/ProductsGrid/index.js
+++ src/components/products/ProductsGrid/index.js
@@ -1,21 +1,63 @@
-import ProductCard from "@/components/products/ProductCard";
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useState, useContext } from "react";
+import { addFavorite } from "@/services/api/wishlist.api.js";
+// import { WishlistContext } from "@/context/WishlistContext";
 
-<<<<<<< HEAD
-const Index = ({ products }) => {
+
+const Index = ({ product }) => {
+    const [isFavorite, setIsFavorite] = useState(false);
+    // const { user } = useContext(WishlistContext);
+
+
+    const toggleFavorite = () => {
+        setIsFavorite(!isFavorite);
+        addFavorite(user.id, product.id);
+    };
     return (
-        <div className="grid grid-cols-4 gap-8 my-12">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-=======
-const Index = () => {
-    return (
-        <div className="grid grid-cols-4 gap-8 my-12">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
->>>>>>> c1aac64 (save wishlist)
+        <div
+            className={`group/card max-w-sm bg-white rounded-lg ${
+                isFavorite ? "favorited" : ""
+            }`}
+        >
+            <Link className="group/thumbnail thumbnail" href="">
+                <div
+                    className={`overflow-hidden w-[250px] h-[300px] relative ${
+                        isFavorite ? "favorited" : ""
+                    } `}
+                >
+                    <Image
+                        className=""
+                        alt="trans"
+                        src="https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&w=600"
+                        fill
+                        sizes="100%"
+                        style={{ objectFit: "cover" }}
+                    />
+                    <div
+                        className="favorite-icon absolute top-0 right-0"
+                        onClick={toggleFavorite}
+                    >
+                        <FavoriteIcon
+                            style={{ color: isFavorite ? "red" : "white" }}
+                        />
+                    </div>
+                </div>
+            </Link>
+            <div className="py-5">
+                <h2 className="text-md mb-3">{product.name}</h2>
+                <p className="font-semibold font-s">{product.price} €</p>
+                <div className="opacity-0 group-hover/card:opacity-100 transition ease-in-out delay-150">
+                    <Link
+                        className="transition ease-in-out delay-150 mt-4 inline-flex items-center px-4 py-3 text-sm border border-slate-500 font-medium text-center text-slate-500 bg-white hover:bg-slate-500 hover:text-white"
+                        href={`/shop/${product.id}`}
+                    >
+                        Voir le produit
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/services/api/product.api.js
+++ src/services/api/product.api.js
@@ -7,9 +7,9 @@ export async function getProducts() {
             },
         });
         const data = await res.json();
+        console.log(data);
         return data.products;
-    }
-    catch (err) {
+    } catch (err) {
         return err;
     }
 }
@@ -21,8 +21,60 @@ export async function getProduct(id) {
         });
         const data = await res.json();
         return data.product;
+    } catch (err) {
+        return err;
     }
-    catch (err) {
+}
+
+export async function createProduct(product) {
+    try {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}api/v1/products`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(product),
+            }
+        );
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        return err;
+    }
+}
+
+export async function updateProduct(id, product) {
+    try {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}api/v1/products/${id}`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(product),
+            }
+        );
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        return err;
+    }
+}
+
+export async function deleteProduct(id) {
+    try {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}api/v1/products/${id}`,
+            {
+                method: "DELETE",
+            }
+        );
+        const data = await res.json();
+        return data;
+    } catch (err) {
         return err;
     }
 }
