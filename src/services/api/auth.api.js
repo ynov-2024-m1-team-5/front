ff9016d49cddf77ad19d9c5ff861dbeff7141a96
Mougni-Nawel <mougninawel@gmail.com>
2024-02-29 23:28:49 +0100

    login integration

--- src/services/api/auth.api.js
+++ src/services/api/auth.api.js
@@ -76,8 +76,34 @@ export async function deleteCustomer(id) {
             method: "DELETE",
         });
         const data = await res.json();
+        router.push('auth/login');
         return data;
     } catch (err) {
         return err;
     }
 }
+
+export async function login(bodyFormData) {
+    console.log("IN : "+JSON.stringify(bodyFormData));
+
+    try {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT_AUTH}token/`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: bodyFormData
+            }
+        );
+
+        const data = await res.json();
+        //window.location.href = "/shop";
+
+        return data;
+    } catch (err) {
+        return err;
+    }
+}
+
