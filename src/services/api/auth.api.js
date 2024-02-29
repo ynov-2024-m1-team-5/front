d00a208d1dd160994341a83e5ba0883db70b2086
Mougni-Nawel <mougninawel@gmail.com>
2024-02-29 22:36:03 +0100

    inscription fonctionnel

--- src/services/api/auth.api.js
+++ src/services/api/auth.api.js
@@ -1,14 +1,16 @@
 export async function saveUser(user) {
     try {
         const res = await fetch(
-            `${process.env.NEXT_PUBLIC_API_ENDPOINT}/products?take=${take}`,
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT_AUTH}customers/`,
             {
-                cache: "no-store",
+                method: "POST",
                 headers: {
-                    "Content-Type": "application/json",
+                    "Content-Type": "application/json"
                 },
+                body: JSON.stringify(user)
             }
         );
+
         const data = await res.json();
         return data;
     } catch (err) {
