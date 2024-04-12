875f82f581c98f184b428081cedd1525cb19f4a8
okabasele <ocekabasele@gmail.com>
2024-04-12 16:52:23 +0200

    📝 🔧 Stripe order webhook and doc

--- src/services/api/order.api.js
+++ src/services/api/order.api.js
@@ -1,16 +1,19 @@
 export async function createOrder(customer_id) {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT_ORDER}${customer_id}`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT_ORDER}/customers/${customer_id}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${localStorage.getItem("token")}`
             }
         });
- 
 
-        // const data = await res.json();
-        // return data;
+        const data = await res.json();
+        console.log({data})
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+        window.location.href = data.url
     } catch (err) {
         return err;
     }
